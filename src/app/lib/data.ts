a226dc526beb92889dd7603758dcc877092f271a
Mirzam Avicena <avicenamirzam@gmail.com>
2024-12-13 03:41:12 +0700

    add select option

---
+++ src/app/lib/data.ts
@@ -0,0 +1,17 @@
+export const wordOptions = [
+  {
+    text: 'rumah',
+    value: 'home',
+    image: 'gambar rumah',
+  },
+  {
+    text: 'buku',
+    value: 'book',
+    image: 'gambar buku',
+  },
+  {
+    text: 'pintu',
+    value: 'door',
+    image: 'gambar pintu',
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,40 @@
 'use client';
 
 import { useState } from 'react';
+import { wordOptions } from './lib/data';
+
+type WordOption = {
+  text: string;
+  value: string;
+  image: string;
+};
 
 export default function Home() {
-  const [selectOption, setSelectOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState<WordOption | null>(null);
 
   const onChange: React.ComponentProps<'select'>['onChange'] = (e) => {
-    setSelectOption(e.target.value);
+    const selectedValue = e.target.value;
+    const option = wordOptions.find((opt) => opt.value === selectedValue) || null;
+    setSelectedOption(option);
   };
 
   return (
     <main>
       <h1>Home</h1>
-      <select onChange={onChange} value={selectOption}>
-        <option value="1">rumah</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
+
+      <select onChange={onChange} value={selectedOption?.value || ''}>
+        <option value="" disabled>
+          Select an option
+        </option>
+        {wordOptions.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.text}
+          </option>
+        ))}
       </select>
 
-      <p>Selected Option: {selectOption}</p>
+      <p>Selected Option: {selectedOption?.value}</p>
+      <p>Image: {selectedOption?.image}</p>
     </main>
   );
 }
