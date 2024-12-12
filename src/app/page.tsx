0df8e08ba7a1a05292fe5d47578b361453d2d869
Mirzam Avicena <avicenamirzam@gmail.com>
2024-12-12 11:30:10 +0700

    fix type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function Home() {
+  const [selectOption, setSelectOption] = useState('');
+
+  const onChange: React.ComponentProps<'select'>['onChange'] = (e) => {
+    setSelectOption(e.target.value);
+  };
+
   return (
     <main>
       <h1>Home</h1>
+      <select onChange={onChange} value={selectOption}>
+        <option value="1">rumah</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+
+      <p>Selected Option: {selectOption}</p>
     </main>
   );
 }
